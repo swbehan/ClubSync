b6778d55983423f91517be6951eba039154d564a
Julian Leonhardt <leonhardtjulian26@gmail.com>
2026-07-13 13:39:21 -0700

    Built out admin dashboard as event managment hub

--- frontend/src/pages/admin/admindashboard/AdminDashboard.jsx
+++ frontend/src/pages/admin/admindashboard/AdminDashboard.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import EventForm from "../../events/event-form/EventForm.jsx";
+import EventList from "../../events/event-list/EventList.jsx";
+
 export default function AdminDashboard() {
+  const [refreshKey, setRefreshKey] = useState(0);
   return (
-    <>
-      <h1>Admin DashBoard</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, illo vero
-        totam sint quas nihil? Commodi, deleniti aspernatur dicta voluptatum
-        enim, soluta provident hic rerum in, impedit eius dolore eum.
+    <Container className="px-5">
+      <h1 className="moto">Event Management</h1>
+      <p className="spacing-after-moto">
+        Create events and manage RSVPs for the active semester.
       </p>
-    </>
+
+      {/* creating an event bumps refreshKey, which remounts the list below */}
+      <EventForm onCreated={() => setRefreshKey((k) => k + 1)} />
+
+      <div className="mt-4">
+        <EventList key={refreshKey} />
+      </div>
+    </Container>
   );
 }
+
